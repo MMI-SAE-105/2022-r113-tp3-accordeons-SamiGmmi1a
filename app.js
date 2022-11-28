d6e7e0c22b690e1a9dce2f7e1ac72d85708d6bef
SamiGmmi1a <sghorzi.univ.fcomte@gmail.com>
2022-11-28 21:57:34 +0100

    onglet java

---
+++ app.js
@@ -0,0 +1,27 @@
+const onglets = document.querySelectorAll('.onglets');
+
+const contenu = document.querySelectorAll('.contenu');
+ 
+let index = 0;
+
+onglets.forEach(onglet  => {
+
+    onglets.addEventListener('click', () => {
+
+        if(onglets.classList.contains('active')){
+            return;
+        } else {
+            onglets.classList.add('active');
+        }
+
+        index = onglets.getAttribute('data-anim');
+        console.log(index);
+
+        for(onglets = 0; onglets < onglets.length; i++) {
+
+            if(onglets[i].getAttribute('data-anim') != index) {
+                onglets[i].classList.remove('active');
+            }
+        }
+    })
+})
